7f18ca615c1f4d178cb6dc19910b05b4ce436b51
Chris Marshall <chris@chrismar035.com>
2014-04-29 02:09:41 -0400

    Fetch books from the database

--- server.go
+++ server.go
@@ -1,11 +1,41 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/go-martini/martini"
+	_ "github.com/lib/pq"
+	"net/http"
+)
+
+func SetupDB() *sql.DB {
+	db, err := sql.Open("postgres", "user=go password=gopass host=localhost dbname=lesson4 sslmode=disable")
+	PanicIf(err)
+
+	return db
+}
+
+func PanicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	m := martini.Classic()
-	m.Get("/", func() (int, string) {
-		return 418, "i'm a teapot"
+	m.Map(SetupDB())
+
+	m.Get("/", func(db *sql.DB, r *http.Request, rw http.ResponseWriter) {
+		rows, err := db.Query("SELECT title, author, description FROM books")
+		PanicIf(err)
+		defer rows.Close()
+
+		var title, author, description string
+		for rows.Next() {
+			err := rows.Scan(&title, &author, &description)
+			PanicIf(err)
+			fmt.Fprintf(rw, "Title: %s\nAuthor: %s\nDescription: %s\n\n", title, author, description)
+		}
 	})
 
 	m.Get("/hello/:name", func(params martini.Params) string {
