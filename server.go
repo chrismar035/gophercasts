4695ae396550f631c605b88295d294bf48231bd8
Chris Marshall <chris@chrismar035.com>
2014-04-29 02:24:30 -0400

    Render HTML templates

--- server.go
+++ server.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"github.com/go-martini/martini"
 	_ "github.com/lib/pq"
 	"net/http"
+	"github.com/martini-contrib/render"
 )
+
+type Book struct {
+	Title		string
+	Author		string
+	Description string
+}
 
 func SetupDB() *sql.DB {
 	db, err := sql.Open("postgres", "user=go password=gopass host=localhost dbname=lesson4 sslmode=disable")
@@ -24,8 +30,11 @@ func PanicIf(err error) {
 func main() {
 	m := martini.Classic()
 	m.Map(SetupDB())
+	m.Use(render.Renderer(render.Options{
+		Layout: "layout",
+	}))
 
-	m.Get("/", func(db *sql.DB, r *http.Request, rw http.ResponseWriter) {
+	m.Get("/", func(db *sql.DB, r *http.Request, ren render.Render) {
 		search := "%" + r.URL.Query().Get("search") + "%"
 		rows, err := db.Query(`SELECT title, author, description FROM books
 					WHERE title ILIKE $1
@@ -34,12 +43,15 @@ func main() {
 		PanicIf(err)
 		defer rows.Close()
 
-		var title, author, description string
+		books := []Book{}
 		for rows.Next() {
-			err := rows.Scan(&title, &author, &description)
+			book := Book{}
+			err := rows.Scan(&book.Title, &book.Author, &book.Description)
 			PanicIf(err)
-			fmt.Fprintf(rw, "Title: %s\nAuthor: %s\nDescription: %s\n\n", title, author, description)
+			books = append(books, book)
 		}
+
+		ren.HTML(200, "books", books)
 	})
 
 	m.Get("/hello/:name", func(params martini.Params) string {
